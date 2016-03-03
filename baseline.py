799c3dc3bde647fd7bfb7c1dc9ed464e1c0cf479
Chris Ying <chrsyng@gmail.com>
2016-03-03 12:10:46 -0500

    pseudo agglomerative clustering

--- baseline.py
+++ baseline.py
@@ -5,6 +5,38 @@ import os
 import numpy as np
 
 DIR = 'ecb/txt/'
+CLUSTERS = 1
+
+class Cluster:
+    def __init__(self, name, vector):
+        self.cluster = [name]
+        self.centroid = vector
+        self.dirty = False      # true after a combine but before an update
+        self.sumv = vector
+        self.numv = 1
+
+    def __str__(self):
+        if self.dirty:
+            self.update()
+        return '(Cluster: %s; Centroid: %s)' % (str(self.cluster), str(self.centroid))
+
+    def combine(self, other):
+        self.cluster += other.cluster
+        self.dirty = True
+        self.sumv += other.sumv
+        self.numv += other.numv
+
+    def update(self):
+        self.centroid = self.sumv / self.numv
+        self.dirty = False
+
+    def distance(self, other):
+        if self.dirty:
+            self.update()
+
+        # Currently Euclidean distance
+        dif = self.centroid - other.centroid
+        return np.dot(dif, dif)
 
 def main():
     events = []
@@ -30,8 +62,24 @@ def main():
 
         events.append((filename, vector))
 
-    print events
+    clusters = [Cluster(filename, vector) for (filename, vector) in events]
+    numClusters = len(events)
+    while (numClusters > CLUSTERS):
+        minPair = None
+        minDist = 2**31 - 1     # A large number
+        for i in range(len(clusters)):
+            for j in range(i+1, len(clusters)):
+                c1 = clusters[i]
+                c2 = clusters[j]
+                dist = c1.distance(c2)
+                if dist < minDist:
+                    minDist = dist
+                    minPair = (i, j)
 
+        (i,j) = (minPair[0], minPair[1])
+        clusters[i].combine(clusters[j])
+        clusters = clusters[:j] + clusters[j+1:]
+        numClusters -= 1
 
 
 if __name__ == '__main__':
